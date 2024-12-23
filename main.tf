e993ffa140e4a97b76737e95400c65f02240240b
vpdevx <vinicpires03@gmail.com>
2024-12-23 15:38:24 -0300

    fix: subnet

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@ resource "aws_subnet" "lab_subnet" {
     availability_zone = "us-east-1a"
 }
 
+resource "aws_subnet" "lab_subnet2" {
+    vpc_id     = aws_vpc.lab_vpc.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1b"
+}
+
 resource "aws_route_table" "lab_route_table" {
     vpc_id = aws_vpc.lab_vpc.id
 
@@ -22,7 +28,7 @@ resource "aws_route_table" "lab_route_table" {
 }
 
 resource "aws_route_table_association" "lab_aws_route_table_association" {
-    subnet_id      = aws_subnet.lab_subnet.id
+    subnet_id      = [aws_subnet.lab_subnet.id, aws_subnet.lab_subnet2.id]
     route_table_id = aws_route_table.lab_route_table.id
   
 }
@@ -71,7 +77,7 @@ resource "aws_lb" "lablb" {
   internal = false
   load_balancer_type = "application"
   security_groups = [aws_security_group.alb_sg.id]
-  subnets = [aws_subnet.lab_subnet.id]
+  subnets = [aws_subnet.lab_subnet.id, aws_subnet.lab_subnet2.id]
 }
 
 resource "aws_lb_target_group" "labtargetgroup" {
