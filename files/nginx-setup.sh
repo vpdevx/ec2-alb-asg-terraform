9eab4be3494c4f535ea9db9f8f2469bf17a16815
vpdevx <vinicpires03@gmail.com>
2024-12-23 16:01:33 -0300

    fix

--- files/nginx-setup.sh
+++ files/nginx-setup.sh
@@ -4,4 +4,5 @@ sudo amazon-linux-extras enable nginx1
 sudo yum install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
-echo "Instance ID: $(curl http://169.254.169.254/latest/meta-data/instance-id)" > /usr/share/nginx/html/index.html
+echo "Instance ID: $(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)" > /usr/share/nginx/html/index.html
+
